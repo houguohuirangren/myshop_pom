0fbb60001ff541a432ebf9d008b288cdff3ed52a
lww <lww>
2018-12-24 11:35:05 +0800

    增加了购物车功能，添加了订单查询，添加订单，订单详情功能

--- myshop_service/src/main/java/com/qf/myshop/myshop_service/MyshopServiceApplication.java
+++ myshop_service/src/main/java/com/qf/myshop/myshop_service/MyshopServiceApplication.java
@@ -4,10 +4,12 @@ import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.qf.dao")
 @DubboComponentScan("com.qf.myshop.myshop_service.serviceImpl")
+@EnableTransactionManagement
 public class MyshopServiceApplication {
 
     public static void main(String[] args) {
